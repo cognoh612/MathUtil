660805cff8c25bd60e794a6e82a7996816c1d4e5
cognoh612 <jiahnguyen@gmail.com>
2020-07-10 17:14:25 +0700

    2nd: note -> edit gi cung duoc

--- src/mathutil/MathUtilMain.java
+++ src/mathutil/MathUtilMain.java
@@ -7,6 +7,8 @@ public class MathUtilMain {
 
     public static void main(String[] args) {
         System.out.println("5! = " + computeFactorial(5));
+        
+        System.out.println("Cai nay tui muon test thu vu git ne");
     }
     
 }
