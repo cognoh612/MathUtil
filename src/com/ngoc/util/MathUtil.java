8ae2c0d8edec913b9c461fe1247756d0e42c0e67
cognoh612 <jiahnguyen@gmail.com>
2020-07-20 17:01:29 +0700

    4th - add the exception handler

--- src/com/ngoc/util/MathUtil.java
+++ src/com/ngoc/util/MathUtil.java
@@ -7,6 +7,10 @@ public class MathUtil {
     //tính n! - tính dưới 15! thôi nha
     public static long computeFactorial(int n) {
         
+        //ham se nem ra ngoai le neu ban dua vao n ca chon
+        if (n < 0 || n > 15)
+            throw new IllegalArgumentException("Invalid argument. n must be >= 0 and <= 15");
+        
         long result = 1;
         for (int i = 1; i <= n; i++) {
             result *= i;
