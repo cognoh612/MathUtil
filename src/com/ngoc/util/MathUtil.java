0f20a785a2d8112fe3ee92d055dfbd53b341cda6
cognoh612 <jiahnguyen@gmail.com>
2020-07-22 17:09:34 +0700

    5th - refactor the cF() method by using the recursion technic

--- src/com/ngoc/util/MathUtil.java
+++ src/com/ngoc/util/MathUtil.java
@@ -11,11 +11,17 @@ public class MathUtil {
         if (n < 0 || n > 15)
             throw new IllegalArgumentException("Invalid argument. n must be >= 0 and <= 15");
         
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+        //choi de quy cho no pro :)
+        if (n == 0 || n == 1)
+            return 1;
+        
+        //bat dau cac lenh con lai o day
+        //dua nao viet else tru diem nha con
+        return n * computeFactorial(n - 1);
     }
     
 }
+
+//nho Jenkins xac nhan code chinh sua co in nhu ngay xua hay ko?
+//Regression dc thuc thi boi Jenkins, test lai nhu gi da tung test
+//de confirm chat luong sau khi bi thay doi
